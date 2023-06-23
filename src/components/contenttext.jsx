01641feefcdcd8c99eeeabc9ebaf5780f707a6df
rebecabrandao1 <rjb@ic.ufal.br>
2023-06-23 13:57:21 -0300

    tst

--- src/components/contenttext.jsx
+++ src/components/contenttext.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { useFirstPrismicDocument } from '@prismicio/react';
+
 
 const ContentText = ({ formularioData }) => {
-  const [document] = useFirstPrismicDocument();
-  console.log(document);
+  
 
   return <h2>Valor do formulário: {formularioData}</h2>;
 };
